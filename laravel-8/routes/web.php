5082049f628bdc61b97189ab2fc27babc3f095f0
RifanRifai <rifanwinprof@gmail.com>
2022-03-27 22:12:15 +0800

    update laravel 8

--- laravel-8/routes/web.php
+++ laravel-8/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MahasiswaControllers;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->group( function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/mahasiswa',[MahasiswaControllers::class, 'index']);
 });
