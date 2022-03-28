ebef4af64725d4b0cce38913fd44eab1ec504fbd
RifanRifai <rifanwinprof@gmail.com>
2022-03-29 07:41:54 +0800

    update

--- laravel-8/resources/views/layouts/menu.blade.php
+++
@@ -1,8 +0,0 @@
-<li class="nav-item">
-    <a href="{{ route('mahsiswassayas.index') }}"
-       class="nav-link {{ Request::is('mahsiswassayas*') ? 'active' : '' }}">
-        <p>Mahsiswassayas</p>
-    </a>
-</li>
-
-
